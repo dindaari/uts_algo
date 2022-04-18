3721e17b17c3943add5563c0c8ae6efc34d783c4
dindaari <dindaat03@gmail.com>
2022-04-18 14:09:37 +0700

    uts 2b

---
+++ facebook.java
@@ -0,0 +1,15 @@
+import Model.FB;
+
+private static void PrintTitle(){
+    System.out.println("---------------------------");
+    System.out.println(" FACEBOOK ");
+    System.out.println("---------------------------");
+}
+public static void main(String[] args) {
+    String user;
+    String foto;
+    String caption;
+    int like;
+    
+
+}
