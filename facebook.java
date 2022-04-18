c2825e82ce210c4b5c6f7317acaccf8fb817b705
dindaari <dindaat03@gmail.com>
2022-04-18 14:28:02 +0700

    uts 2b

--- facebook.java
+++ facebook.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 import Model.FB;
 
-private static void PrintTitle(){
+private static PrintTitle(){
     System.out.println("---------------------------");
     System.out.println(" FACEBOOK ");
     System.out.println("---------------------------");
@@ -9,7 +10,16 @@ public static void main(String[] args) {
     String user;
     String foto;
     String caption;
+    int password;
     int like;
+    int koment;
     
+    user = "Dindari";
+    password = "123";
+    Scanner inputUser = new Scanner(System.in);
+    PrintTitle();
+    System.out.println("Masukan User");
+    System.out.println("Masukan Password");
+    Password = inputUser.nextLine();
 
 }
